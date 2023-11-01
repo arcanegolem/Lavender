5b650e79fd8fad7ba66f737c80060ce782008569
arcanegolem <dopeflipper@gmail.com>
2023-11-01 15:18:54 +0300

    Refactor

--- lavender/src/main/java/ru/spektrit/lavender/NfcTagHelper.kt
+++ lavender/src/main/java/ru/spektrit/lavender/NfcTagHelper.kt
@@ -2,6 +2,6 @@ package ru.spektrit.lavender
 
 interface NfcTagHelper {
    fun readConfig() : Map<String, Byte>
-   fun auth(pass : ByteArray)
+   fun auth(pwd : ByteArray)
    fun getTagInformationAsString() : String
 }
